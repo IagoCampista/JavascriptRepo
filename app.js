563f1e6b5498dd219bc256773615ebdbfbc3e721
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-05 17:31:50 -0300

    Ternary Operator

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const name = 'John';
-const age = 25;
-const sentence = "Hey it's " + name + ' and he is ' + age + ' years old';
-console.log(sentence);
-//são equivalentes
-const senteceBacktick = `Hey it's ${name} and he is ${age} years old`;
-console.log(senteceBacktick);
+//Ternary Operator - Operador Ternário
+const value = 1 < 0;
+
+if (value){
+    console.log('value is true');
+} else{
+    console.log('value is false');
+}
+
+//é equivalente a:
+
+value ? console.log('value is true') : console.log('value is false');
