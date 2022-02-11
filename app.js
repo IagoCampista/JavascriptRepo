b12cc7d615de6f6d6cd5286e04269533fa508df0
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-11 09:42:34 -0300

    className e classList

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const first = document.querySelector('.first');//seleciona o elemento
-const idValue = first.getAttribute('id');// com o elemento em "maos", temos acesso aos outro atibutos dele
-console.log(idValue);// É printado  - special -
+//clasName
+//classList
+
+const first = document.getElementById('first');//seleciona o elemento
+const classValue = first.className; //obtem a classe dele
+console.log(classValue); //é printado "text"
 
 
-const link = document.getElementById('link');//seleciono o elemento
-const showLink = link.getAttribute('href');//seleciona o atributo do elemento
-console.log(showLink);//mostra o atributo em questao; é printado "first.html"
-
-const last = link.nextElementSibling; //seleciona o elemento nesse caso o logo apos o link, "o 2 li"
-last.setAttribute('class', 'first'); // "seta" uma classe chamada first a este elemento
-last.textContent = `i also have a class of first`; //mostrando que tbm é possível mudar o valor do texto do elemento
-console.log(last);// é printado um li com a classe first, conforme setadop acima
+const second = document.getElementById('second');//seleciona o elemento
+second.className = 'colors text'; // é possível adicionar mais de uma classe a um mesmo elemento com o className, mas isso nao é remonedado
 
 
-const links = document.querySelectorAll('.first'); //seleciona todos os elemento com classe "first" nele
-console.log(links); // nesse caso mostra 2 li, 1 com id special e o outro nao conforme feita as alteracoes acima
+const third = document.getElementById('third'); //seleciona o elemento
+third.classList.add('text', 'colors'); // melhor solucao para adicionar e modificar classes de um elemento. basta separalos por ","
+third.classList.remove('text'); // para remover uma classe, tbm funciona com multiplas, usando as ","
+let result = third.classList.contains('colors'); //retorna um boolenao com vdd se o elemento em questao tiver a classe passada como argumento
+console.log(result); // printa true
