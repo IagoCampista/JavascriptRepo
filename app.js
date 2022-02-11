8740b7feb092be9d1896aa7842c3caab8201d1a2
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-10 22:53:36 -0300

    getAttribute e setAttibute

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-//Ternary Operator - Operador Ternário
-const value = 1 < 0;
+const first = document.querySelector('.first');//seleciona o elemento
+const idValue = first.getAttribute('id');// com o elemento em "maos", temos acesso aos outro atibutos dele
+console.log(idValue);// É printado  - special -
 
-if (value){
-    console.log('value is true');
-} else{
-    console.log('value is false');
-}
 
-//é equivalente a:
+const link = document.getElementById('link');//seleciono o elemento
+const showLink = link.getAttribute('href');//seleciona o atributo do elemento
+console.log(showLink);//mostra o atributo em questao; é printado "first.html"
 
-value ? console.log('value is true') : console.log('value is false');
+const last = link.nextElementSibling; //seleciona o elemento nesse caso o logo apos o link, "o 2 li"
+last.setAttribute('class', 'first'); // "seta" uma classe chamada first a este elemento
+last.textContent = `i also have a class of first`; //mostrando que tbm é possível mudar o valor do texto do elemento
+console.log(last);// é printado um li com a classe first, conforme setadop acima
+
+
+const links = document.querySelectorAll('.first'); //seleciona todos os elemento com classe "first" nele
+console.log(links); // nesse caso mostra 2 li, 1 com id special e o outro nao conforme feita as alteracoes acima
