1b65c918fa2a075eddea4ba60f7404b2a5c68a3f
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-22 18:05:58 -0300

    remove

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-//prepend - place the element in the beginning of a referal element
-//innerText
+//remove
+const result = document.querySelector('#result'); //seleciona a div com id = result
+result.remove(); // apaga todo o elemento selecionado (nesse caso a div)
 
-const heading = document.createElement('h2'); //cria um elemento h2 vazio
-heading.innerText = `i am a dynamic heading`; // seta o texto do elemento criado heading como a string informada
-document.body.prepend(heading); //coloca o elemento criado headinf no inicio do body
+
