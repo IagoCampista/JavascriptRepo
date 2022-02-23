4f39619515871460c1967f5792df029682b1c045
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-22 21:31:07 -0300

    innerHTML

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-//removeChild
-const result = document.querySelector('#result'); //seleciona a div com id = result
-const heading = result.querySelector('h1'); //seleciona o h1 dentro do result
-result.removeChild(heading); // remove o filho headind do elemento pai result
+//innerHTML
 
+const list = document.getElementById('first');
+const div = document.getElementById('second');
+const item = document.querySelector('.item');
 
+console.log(list.textContent);
+
+const randomVar = 'random item list';
+
+const ul = document.createElement('ul'); // cria um elemento ul vazio
+ul.innerHTML = `<li class="item">${randomVar}
+<li>list item 2</li> <li>list item 3</li>`; // edita o html do elemento e usando os backticks podemos ate incluir variaveis
+document.body.appendChild(ul); // adiciona o item criado no final do body 
