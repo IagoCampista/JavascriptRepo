37d6e7b9ee0cc3778acaff69d47fda7c26d4d9f1
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-02-28 20:22:51 -0300

    Pure Color Flipper finished - No CSS

--- projects/ColorFlipper/app.js
+++ projects/ColorFlipper/app.js
@@ -1,7 +1,14 @@
 const colors = ['red', 'green', 'blue', 'grey', 'yellow', 'rgba(123,098,245)', '#f0f5f9', '#6d32a8', 'rgba(50,148,168)'];
 const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+console.log(color);
 
 btn.addEventListener('click', function() {
     let randomNumber = Math.floor(Math.random() *colors.length);
     document.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = colors[randomNumber];
  });
+
+ //parei em 20:12 mas falta arrumar o css
