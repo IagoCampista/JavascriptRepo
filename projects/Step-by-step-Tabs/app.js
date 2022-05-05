6d55a400fe2bd9fa2d5c67cac179098219c396c8
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-05-05 15:33:04 -0300

    Selected Options Summary done

--- projects/Step-by-step-Tabs/app.js
+++ projects/Step-by-step-Tabs/app.js
@@ -1,4 +1,25 @@
-const size = document.querySelector("#size");
-const text = size.options[size.selectedIndex].text;
+function update() {
+  // size
+  const size = document.getElementById("size");
+  let selectedSizeText = size.options[size.selectedIndex].text;
 
-console.log(text);
+  const sizePreview = document.getElementById("selected-size");
+  sizePreview.textContent = selectedSizeText;
+
+  //paper type
+  const paperType = document.getElementById("paper-type");
+  let selectedPaperTypeText = paperType.options[paperType.selectedIndex].text;
+
+  const paperTypePreview = document.getElementById("selected-paper-type");
+  paperTypePreview.textContent = selectedPaperTypeText;
+
+  //cover color
+  const coverColor = document.getElementById("cover-color");
+  let selectedCoverColorText =
+    coverColor.options[coverColor.selectedIndex].text;
+
+  const coverColorPreview = document.getElementById("selected-cover-color");
+  coverColorPreview.textContent = selectedCoverColorText;
+}
+
+update();
