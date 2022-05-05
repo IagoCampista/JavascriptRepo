30948d092e7a345a09b43af15a0d64a4096f7d86
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-05-05 16:51:44 -0300

    Simple Step-by-Step done

--- projects/Step-by-step-Tabs/app.js
+++ projects/Step-by-step-Tabs/app.js
@@ -1,3 +1,4 @@
+//Atualiza o Resumo no final da pagina
 function update() {
   // size
   const size = document.getElementById("size");
@@ -23,3 +24,56 @@ function update() {
 }
 
 update();
+
+// Cria controle de Etapas
+let stepCounter = 0;
+const currentStepText = document.getElementById("currentStepText");
+
+//selecionas as divs dos passos
+const steps = document.querySelectorAll(".steps");
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const buttonType = e.currentTarget.classList;
+    if (buttonType.contains("previous")) {
+      stepCounter--;
+    } else if (buttonType.contains("next")) {
+      stepCounter++;
+    } else {
+      stepCounter = 0;
+    }
+
+    if (stepCounter > 3) {
+      stepCounter = 3;
+    } else if (stepCounter < 1) {
+      stepCounter = 1;
+    }
+
+    if (stepCounter == 1) {
+      // mostra a div do passo 1
+      steps.forEach(function (step) {
+        step.classList.remove("active");
+        if (step.classList.contains("step1")) {
+          step.classList.add("active");
+        }
+      });
+    } else if (stepCounter == 2) {
+      steps.forEach(function (step) {
+        step.classList.remove("active");
+        if (step.classList.contains("step2")) {
+          step.classList.add("active");
+        }
+      });
+    } else if (stepCounter == 3) {
+      steps.forEach(function (step) {
+        step.classList.remove("active");
+        if (step.classList.contains("step3")) {
+          step.classList.add("active");
+        }
+      });
+    }
+
+    currentStepText.textContent = stepCounter;
+  });
+});
